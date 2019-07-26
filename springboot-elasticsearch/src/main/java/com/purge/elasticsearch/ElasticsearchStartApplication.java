821925bf981eb2c8678caf0d171a5cc32b265051
Purgeyao <zhaoy@deepblueai.com>
2019-07-26 21:54:03 +0800

    create project

---
+++ springboot-elasticsearch/src/main/java/com/purge/elasticsearch/ElasticsearchStartApplication.java
@@ -0,0 +1,13 @@
+package com.purge.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchStartApplication.class, args);
+    }
+
+}
